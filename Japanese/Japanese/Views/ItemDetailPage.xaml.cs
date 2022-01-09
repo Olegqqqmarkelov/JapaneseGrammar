c3d0c1c86d74b1ca25de68077f82238ad0b46fea
Olegqqqmarkelov <62740940+Olegqqqmarkelov@users.noreply.github.com>
2022-01-09 14:25:06 +0200

    Add project files.

---
+++ Japanese/Japanese/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Japanese.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Japanese.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Japanese/Japanese/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Japanese.Models;
+using Japanese.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Japanese.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
